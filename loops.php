b62c42fec32bf727d77bbbbb32e69cb7da85b356
AS3-2-05 <AS3-2-05@uni.lan>
2018-09-24 13:02:30 +0200

    array

--- loops.php
+++ loops.php
@@ -4,8 +4,21 @@
 
 //print "a" 100 veces
 
+$x = 1; 
 
+while($x <= 100) {
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+
+/*
+$num = 0;
 for ($x = 0; $x <= 100; $x++) {
+    $num++;
+    echo "buelta" .$num. ".";
     echo "a";
+    echo "<br>";
 } 
 ?>
+*/
