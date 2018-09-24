2cbe8af23a70c300355eb48358202e1a1f62876f
AS3-2-05 <AS3-2-05@uni.lan>
2018-09-24 13:48:08 +0200

    fdg

--- array.php
+++ array.php
@@ -1,4 +1,13 @@
 <?php
 $cars = array("Volvo", "BMW", "Toyota");
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+for ($x = 0; $x < count($cars); $x++) {
+    echo $cars[$x];
+    echo "<br>";
+}
+
+foreach($cars as $coche) {
+    echo $coche;
+    echo "<br>";
+}
 ?>
