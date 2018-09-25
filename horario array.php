6b4f862a3d95f7cfdd20806c22d94264f298dc6e
AS3-2-05 <AS3-2-05@uni.lan>
2018-09-25 08:41:30 +0200

    fbf

---
+++ horario array.php
@@ -0,0 +1,11 @@
+<?php
+
+$horario = array(
+    array("BASE DE DATOS","EIE","SO","SO","WEB","WEB"), //LUNES
+    array("WEB","SEGURIDAD","REDES","REDES","BASE DE DATOS","BASE DE DATOS"), //MARTES
+    array("SEGURIDAD","SEGURIDAD","SO","SO","EIE","EIE"), //MIERCOLES
+    array("REDES","REDES","SO","SO","WEB","WEB"), //JUEVES
+    array("SEGURIDAD","SEGURIDAD","REDES","REDES","INGLES","INGLES"), //VIERNES
+);
+
+echo $horario[2][0];
