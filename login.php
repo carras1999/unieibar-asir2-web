ea3a6440ebffcab85161a386de0b501eefe5044e
AS3-2-05 <AS3-2-05@uni.lan>
2018-10-04 13:56:19 +0200

    ghcj

--- login.php
+++ login.php
@@ -3,3 +3,14 @@
 echo $_POST["userid"];
 echo "<br>";
 echo $_POST["psw"];
+
+$servername = "localhost";
+$username = "ivan";
+$password = "admin";
+$database = "web";
+
+echo $_POST["userid"];
+echo "<br>";
+echo $_POST["psw"];
+
+$conn = new mysqli($servername, $username, $password,$database);
