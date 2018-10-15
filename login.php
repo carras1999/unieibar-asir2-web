3b99bb0f61a6702fffcc60803faec4225b1b56da
AS3-2-05 <AS3-2-05@uni.lan>
2018-10-15 13:08:58 +0200

    dhvbk

--- login.php
+++ login.php
@@ -1,16 +1,42 @@
 <?php
 
-echo $_POST["userid"];
-echo "<br>";
-echo $_POST["psw"];
+$user = $_POST["userid"];
+$passw = $_POST["psw"];
 
 $servername = "localhost";
-$username = "ivan";
-$password = "admin";
+$username = "root";
+$password = "";
 $database = "web";
 
-echo $_POST["userid"];
+/*echo $_POST["userid"];
 echo "<br>";
 echo $_POST["psw"];
+echo "<br>";
+*/
+$conn = mysqli_connect($servername, $username, $password,$database);
 
-$conn = new mysqli($servername, $username, $password,$database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+echo "<br>";
+
+$query = "SELECT * FROM users WHERE user='$user' AND password='$passw'";
+//echo $query;
+
+$result = mysqli_query($conn, $query);
+//var_dump($result);
+
+$row  = mysqli_fetch_array($result);
+//echo $row[0];
+//var_dump($row[0]);
+
+if ($row) {
+    echo "Login OK";
+} else {
+    //echo "Login KO";
+    header('Location: '."login.html");
+}
+
+?>
